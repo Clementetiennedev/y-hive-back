5eb87de2d84295e09e2d9137bd4fa06de9f8deb8
romainsilvy <romain@qr-developpement.fr>
2025-02-14 14:00:54 +0100

    :zap: add apiaries

--- app/Models/Apiary.php
+++ app/Models/Apiary.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -19,6 +20,15 @@ class Apiary extends Model
         'updated_at',
         'deleted_at',
     ];
+
+    protected static function booted()
+    {
+        static::addGlobalScope('belongsToUser', function (Builder $builder) {
+            if (auth()->check()) {
+                $builder->where('user_id', auth()->id());
+            }
+        });
+    }
 
     /**
      * hives
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ApiaryController;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,15 +15,15 @@ Route::post('login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('user', [AuthController::class, 'me']);
     Route::post('logout', [AuthController::class, 'logout']);
-});
 
-// Route::prefix('user')
-//         ->controller(UserController::class)
-//         ->name('user.')
-//         ->group(function () {
-//             Route::post('register', 'register')->name('register');
-//             Route::post('edit', 'update')->name('update');
-//             Route::get('me', 'show')->name('show');
-//             Route::get('logout', 'logout')->name('logout');
-//             Route::post('login', 'login')->name('login');
-//         });
+    Route::prefix('apiary')
+        ->controller(ApiaryController::class)
+        ->name('apiary.')
+        ->group(function () {
+            Route::get('index', 'index')->name('index');
+            Route::get('{apiary}', 'show')->name('show');
+            Route::post('store', 'store')->name('store');
+            Route::post('{apiary}', 'update')->name('update');
+            Route::delete('{apiary}', 'destroy')->name('destroy');
+        });
+});
