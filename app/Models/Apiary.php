672f835df1690fe82927af07afea89dd8818fb79
Quentin SAILLARD <saillardq@gmail.com>
2025-03-03 15:17:50 +0100

    :zap: create HiveFactory, ApiaryFactory to the seeder

--- app/Models/Apiary.php
+++ app/Models/Apiary.php
@@ -3,12 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Apiary extends Model
 {
+    /**
+     * @use HasFactory<\Database\Factories\ApiaryFactory>
+     */
+    use HasFactory;
+
     use SoftDeletes;
 
     protected $fillable = [
