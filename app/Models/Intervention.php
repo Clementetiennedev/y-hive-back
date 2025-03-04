dc7cc024846a55c92490c2307e1cf41cf367d591
Quentin SAILLARD <saillardq@gmail.com>
2025-03-04 10:06:33 +0100

    :zap: create Interventionfacotry

--- app/Models/Intervention.php
+++ app/Models/Intervention.php
@@ -2,11 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Intervention extends Model
 {
+    /**
+     * @use HasFactory<\Database\Factories\InterventionFactory>
+     */
+    use hasFactory;
+
     use SoftDeletes;
 
     protected $fillable = [
